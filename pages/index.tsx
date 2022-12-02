30244422fcb5018be72f68c04e214570ec399a22
kento <kento@toriwatari.bird>
2022-12-02 21:39:11 +0900

    ✨ Update

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,76 +1,83 @@
+import React from "react"
 import type { NextPage } from 'next'
 
 import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
 
+import Header from './components/Header';
+
 import * as Styles from "./styles/contribute.module.css"
 
 const Home: NextPage = ({ data }: any) => (
-  /* @ts-ignore */
-  <div className={Styles.wrapper}>
-    {data.user.contributionsCollection.contributionCalendar.weeks.map((week: any, i: any) => (
-      <div
-        key={`week${i}`}
-        /* @ts-ignore */
-        className={Styles.week}
-      >
-        {week.contributionDays.map((day: any, i: any) => (
-          <div key={`keyday${i}`}>
-            {(() => {
-              if (day.contributionCount >= 14) {
-                return (
-                  <div
-                    key={`day5${i}`}
-                    /* @ts-ignore */
-                    className={`${Styles.green5} ${Styles.box}`}
-                  ></div>
-                )
-              } else if (day.contributionCount >= 10) {
-                return (
-                  <div
-                    key={`day4${i}`}
-                    /* @ts-ignore */
-                    className={`${Styles.green4} ${Styles.box}`}
-                  ></div>
-                )
-              } else if (day.contributionCount >= 6) {
-                return (
-                  <div
-                    key={`day3${i}`}
-                    /* @ts-ignore */
-                    className={`${Styles.green3} ${Styles.box}`}
-                  ></div>
-                )
-              } else if (day.contributionCount >= 2) {
-                return (
-                  <div
-                    key={`day2${i}`}
-                    /* @ts-ignore */
-                    className={`${Styles.green2} ${Styles.box}`}
-                  ></div>
-                )
-              } else if (day.contributionCount === 1) {
-                return (
-                  <div
-                    key={`day1${i}`}
-                    /* @ts-ignore */
-                    className={`${Styles.green1} ${Styles.box}`}
-                  ></div>
-                )
-              } else {
-                return (
-                  <div
-                    key={`day0${i}`}
-                    /* @ts-ignore */
-                    className={`${Styles.none} ${Styles.box}`}
-                  ></div>
-                )
-              }
-            })()}
-          </div>
-        ))}
-      </div>
-    ))}
-  </div>
+  <>
+    <Header />
+
+    {/*<div className={Styles.wrapper}>*/}
+    <div>
+      {data.user.contributionsCollection.contributionCalendar.weeks.map((week: any, i: any) => (
+        <div
+          key={`week${i}`}
+          /* @ts-ignore */
+          className={Styles.week}
+        >
+          {week.contributionDays.map((day: any, i: any) => (
+            <div key={`keyday${i}`}>
+              {(() => {
+                if (day.contributionCount >= 14) {
+                  return (
+                    <div
+                      key={`day5${i}`}
+                      /* @ts-ignore */
+                      className={`${Styles.green5} ${Styles.box}`}
+                    ></div>
+                  )
+                } else if (day.contributionCount >= 10) {
+                  return (
+                    <div
+                      key={`day4${i}`}
+                      /* @ts-ignore */
+                      className={`${Styles.green4} ${Styles.box}`}
+                    ></div>
+                  )
+                } else if (day.contributionCount >= 6) {
+                  return (
+                    <div
+                      key={`day3${i}`}
+                      /* @ts-ignore */
+                      className={`${Styles.green3} ${Styles.box}`}
+                    ></div>
+                  )
+                } else if (day.contributionCount >= 2) {
+                  return (
+                    <div
+                      key={`day2${i}`}
+                      /* @ts-ignore */
+                      className={`${Styles.green2} ${Styles.box}`}
+                    ></div>
+                  )
+                } else if (day.contributionCount === 1) {
+                  return (
+                    <div
+                      key={`day1${i}`}
+                      /* @ts-ignore */
+                      className={`${Styles.green1} ${Styles.box}`}
+                    ></div>
+                  )
+                } else {
+                  return (
+                    <div
+                      key={`day0${i}`}
+                      /* @ts-ignore */
+                      className={`${Styles.none} ${Styles.box}`}
+                    ></div>
+                  )
+                }
+              })()}
+            </div>
+          ))}
+        </div>
+      ))}
+    </div>
+  </>
 )
 
 export default Home
