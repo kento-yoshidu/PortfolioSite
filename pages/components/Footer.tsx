62e51b26c8ebec16795551bb89ee1fae43ee86b5
kento <kento@toriwatari.bird>
2022-12-08 21:18:43 +0900

    🎨 CSS更新

--- pages/components/Footer.tsx
+++ pages/components/Footer.tsx
@@ -1,8 +1,26 @@
 import Styles from "../styles/footer.module.scss"
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faGithub } from "@fortawesome/free-brands-svg-icons"
+
+import "@fortawesome/fontawesome-svg-core/styles.css"
+import { config } from "@fortawesome/fontawesome-svg-core"
+config.autoAddCss = false
+
 const Footer = () => (
   <footer className={Styles.footer}>
-    <h2 className={Styles.footerTitle}>Developed by Gatsby</h2>
+
+    <a
+      href="https://github.com/kento-yoshidu/PortfolioSite"
+      className={Styles.link}
+    >
+      <FontAwesomeIcon
+        className={Styles.icon}
+        icon={faGithub}
+      />
+    </a>
+
+    <h2 className={Styles.footerTitle}>Developed by Next.js</h2>
   </footer>
 )
 
