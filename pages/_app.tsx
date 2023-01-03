dd5914d58dcb78680a3c96529fa7d7553482f09e
kento <kento@toriwatari.bird>
2023-01-03 13:39:15 +0900

    👨‍⚕️ Bugfix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,7 @@ import type { AppProps } from 'next/app'
 import { ApolloProvider } from '@apollo/client'
 import client from '../apollo-client'
 
-import "./scss/global.css"
+import "./css/global.css"
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
