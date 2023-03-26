d7d9330e5e8ab1ab81a2aa3ba148cee2e5c99672
kento <kento@toriwatari.bird>
2023-03-26 12:02:43 +0900

    ✨ #109 Setting Storybook

---
+++ pages/components/appsAndSites.stories.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import AppsAndSites from './AppsAndSites'
+
+export default {
+  title: 'Example/AppsAndSites',
+  component: AppsAndSites,
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+} as ComponentMeta<typeof AppsAndSites>
+
+const Template: ComponentStory<typeof AppsAndSites> = (args) => <AppsAndSites {...args} />
+
+export const Primary = Template.bind({})
+Primary.args = {
+  url: "https://google.com",
+  title: "dummy",
+  text: "hogehoge",
+  technologies: ["HTML", "CSS"]
+}
